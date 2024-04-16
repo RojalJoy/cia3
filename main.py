4117ac46f824e9f1daafd174d53d47ed42ccce1d
RojalJoy <141493899+RojalJoy@users.noreply.github.com>
2024-04-16 10:00:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import streamlit as st
-
-pages = {
-    "Home": "home.py",
-    "Page 1": "page1.py",
-    "Page 2": "page2.py",
-    "Page 3":"page3.py",
-    "Page 4":"page4.py",
-    "Page 5":"page5.py"
+pages={
+    "Home":"home.py",
+    "3D Plot Visualization":"page1.py",
+    "Image Processing":"page2.py",
+    "Text Similarity" :"page3.py"
 }
-
 
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(pages.keys()))
@@ -18,3 +14,33 @@ page_to_load = pages[selection]
 with open(page_to_load, "r") as f:
     code = f.read()
 exec(code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
