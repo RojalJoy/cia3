6aab270f7ca0ebd3f5d5305203b7d992f1239826
RojalJoy <141493899+RojalJoy@users.noreply.github.com>
2024-04-16 10:01:34 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,15 +1,13 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
-# Load the dataset
-df = pd.read_csv('WomensClothingE-CommerceReviews - WomensClothingE-CommerceReviews.csv')
+def run():
+  
 
-# Create a histogram of ratings
-st.title('Distribution of Ratings')
-rating_counts = df['Rating'].value_counts()
-fig, ax = plt.subplots()
-ax.bar(rating_counts.index, rating_counts.values)
-plt.xlabel('Rating')
-plt.ylabel('Count')
-st.pyplot(fig)
+    st.header("Welcome !")
+    st.subheader("Womens Clothing E commerce dataset analysis")
+
+
+    
+
+if __name__ == "__main__":
+    run()
